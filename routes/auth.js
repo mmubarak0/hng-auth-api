db7fa59bf13f4a0899a5a33441a037785d6d5882
Mohammed Mubarak <modymu9@gmail.com>
2024-07-08 11:25:44 +0200

    database: deleted old unsed references to mongoose to avoid confusion

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const pool = require('../db');
